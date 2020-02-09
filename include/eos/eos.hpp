392abbe7306036cb614aeb891630ecb78ac0934b
Sho Hirose <sho.hirose@gmail.com>
2020-02-09 23:30:16 +0900

    style: rename lucas_method.hpp as lucas.hpp

--- include/eos/eos.hpp
+++ include/eos/eos.hpp
@@ -23,7 +23,7 @@
 #pragma once
 
 #include "eos/flash.hpp"
-#include "eos/lucas_method.hpp"
+#include "eos/lucas.hpp"
 #include "eos/pr_eos.hpp"
 #include "eos/srk_eos.hpp"
 #include "eos/vdw_eos.hpp"
