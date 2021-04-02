134c14ad3f473af06826e757ef9f02639cff7b1f
Sho Hirose <sho.hirose@gmail.com>
2021-04-02 19:25:49 +0900

    refactor: Use std::complex in real_roots

--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -17,19 +17,22 @@ namespace eos
     template <int N>
     std::vector<double> real_roots(const std::array<double, N> &a) noexcept
     {
-        std::array<double, 2 * (N - 1)> z;
+        // Array-oriented access of the array of std::complex is guranteed.
+        // Please refer to
+        // https://en.cppreference.com/w/cpp/numeric/complex
+        std::array<std::complex<double>, N - 1> z;
         auto *w = gsl_poly_complex_workspace_alloc(N);
-        gsl_poly_complex_solve(a.data(), N, w, z.data());
+        gsl_poly_complex_solve(a.data(), N, w, reinterpret_cast<double*>(z.data()));
         gsl_poly_complex_workspace_free(w);
 
         std::vector<double> x;
         x.reserve(N - 1);
-        constexpr auto tolerance = 1e-8;
+        constexpr double tolerance = 1e-8;
         for (int i = 0; i < N - 1; ++i)
         {
-            if (std::fabs(z[2 * i + 1]) < tolerance)
+            if (std::fabs(z[i].imag()) < tolerance)
             {
-                x.push_back(z[2 * i]);
+                x.push_back(z[i].real());
             }
         }
         std::sort(x.begin(), x.end());
