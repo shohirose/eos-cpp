3f6cf5a5bb632904e0b6046d8728ac55b6212f3b
Sho Hirose <sho.hirose@gmail.com>
2021-04-02 18:01:48 +0900

    feat: Create real_roots for polynomials with N coefficients

--- include/eos/math/polynomial.hpp
+++ include/eos/math/polynomial.hpp
@@ -17,4 +17,18 @@ namespace eos
   /// \f]
   std::vector<double> real_roots(double a, double b, double c) noexcept;
 
+  /// @brief Computes real roots of a polynomial with N coefficients
+  /// @tparam N The number of coefficients
+  /// @param[in] a The coefficients of the polynomial
+  /// @returns Real roots in the ascending order
+  ///
+  /// The polynomial takes the form of:
+  /// \f[
+  ///  a[0] + a[1] x + a[2] x^2 + ... + a[N-1] x^{N-1} = 0
+  /// \f]
+  ///
+  /// N = 4, 5, and 6 are available.
+  template <int N>
+  std::vector<double> real_roots(const std::array<double, N>& a) noexcept;
+
 } // namespace eos
--- src/polynomial.cpp
+++ src/polynomial.cpp
@@ -1,4 +1,5 @@
 #include "eos/math/polynomial.hpp"
+#include <complex>
 #include <gsl/gsl_poly.h>
 
 namespace eos
@@ -11,5 +12,30 @@ namespace eos
         x.resize(num_roots);
         return x;
     }
+
+    template <int N>
+    std::vector<double> real_roots(const std::array<double, N> &a) noexcept
+    {
+        std::array<double, 2 * (N - 1)> z;
+        auto *w = gsl_poly_complex_workspace_alloc(N);
+        gsl_poly_complex_solve(a.data(), N, w, z.data());
+        gsl_poly_complex_workspace_free(w);
+
+        std::vector<double> x(N - 1);
+        constexpr auto tolerance = 1e-8;
+        for (int i = 0; i < N - 1; ++i)
+        {
+            if (std::fabs(z[2 * i + 1]) < tolerance)
+            {
+                x.push_back(z[2 * i]);
+            }
+        }
+
+        return x;
+    }
+
+    template std::vector<double> real_roots<4>(const std::array<double, 4> &a) noexcept;
+    template std::vector<double> real_roots<5>(const std::array<double, 5> &a) noexcept;
+    template std::vector<double> real_roots<6>(const std::array<double, 6> &a) noexcept;
 
 } // namespace eos
