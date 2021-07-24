e9fe214a71a9f67e65b8272776efdc48e6147f59
Sho Hirose <sho.hirose@gmail.com>
2021-07-25 00:04:21 +0900

    Update comments for SoaveRedlichKwongEos

--- include/eos/soave_redlich_kwong_eos.hpp
+++ include/eos/soave_redlich_kwong_eos.hpp
@@ -8,52 +8,73 @@
 
 namespace eos {
 
-/// @brief Soave-Redlich-Kwong EoS.
+/**
+ * @brief EoS policy for Soave-Redlich-Kwong EoS.
+ *
+ * @tparam Scalar scalar
+ */
 template <typename Scalar>
 struct SoaveRedlichKwongEosPolicy {
+  /// Coefficient for attraction parameter
   static constexpr Scalar omegaA = 0.42748;
+  /// Coefficient for repulsion parameter
   static constexpr Scalar omegaB = 0.08664;
 
-  // Static functions
+  /// @name Public static functions
+  //@{
 
-  /// @brief Computes pressure at given temperature and volume
-  /// @param[in] t Temperature
-  /// @param[in] v Volume
-  /// @param[in] a Attraction parameter
-  /// @param[in] b Repulsion parameter
-  /// @returns Pressure
+  /**
+   * @brief Compute pressure at given temperature and volume
+   *
+   * @param t temperature
+   * @param v volume
+   * @param a attraction parameter
+   * @param b repulsion parameter
+   * @return Scalar pressure
+   */
   static Scalar pressure(const Scalar& t, const Scalar& v, const Scalar& a,
                          const Scalar& b) noexcept {
     constexpr auto R = gasConstant<Scalar>();
     return R * t / (v - b) - a / (v * (v + b));
   }
 
-  /// @brief Computes the coefficients of the cubic equation of z-factor.
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
-  /// @returns Coefficients of the cubic equation of z-factor.
+  /**
+   * @brief Compute coefficients of the cubic equation of Z-factor.
+   *
+   * @param a reduced attraction parameter
+   * @param b reduced repulsion parameter
+   * @return std::array<Scalar, 3> coefficients of the cubic equation of
+   * Z-factor
+   */
   static std::array<Scalar, 3> zfactorCubicEq(const Scalar& a,
                                               const Scalar& b) noexcept {
     return {-1, a - b - b * b, -a * b};
   }
 
-  /// @brief Computes the natural logarithm of a fugacity coefficient
-  /// @param[in] z Z-factor
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
-  /// @returns The natural logarithm of a fugacity coefficient
+  /**
+   * @brief Computes natural log of fugacity coefficients
+   *
+   * @param z Z-factor
+   * @param a reduced attraction parameter
+   * @param b reduced repulsion parameter
+   * @return Scalar natural log of fugacity coefficients
+   */
   static Scalar lnFugacityCoeff(const Scalar& z, const Scalar& a,
                                 const Scalar& b) noexcept {
     using std::log;
     return z - 1 - log(z - b) - a / b * log((z + b) / z);
   }
 
-  /// @brief Computes residual enthalpy
-  /// @param[in] z Z-factor
-  /// @param[in] t Temperature
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
-  /// @param[in] beta Temperature correction factor
+  /**
+   * @brief Compute residual enthalpy
+   *
+   * @param z Z-factor
+   * @param t temperature
+   * @param a reduced attraction parameter
+   * @param b reduced repulsion parameter
+   * @param beta derivative of correction factor
+   * @return Scalar residual enthalpy
+   */
   static Scalar residualEnthalpy(const Scalar& z, const Scalar& t,
                                  const Scalar& a, const Scalar& b,
                                  const Scalar& beta) noexcept {
@@ -62,10 +83,15 @@ struct SoaveRedlichKwongEosPolicy {
     return R * t * (z - 1 - a / b * (1 - beta) * log((z + b) / z));
   }
 
-  /// @brief Computes residual entropy
-  /// @param[in] z Z-factor
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
+  /**
+   * @brief Compute residual entropy
+   *
+   * @param z Z-factor
+   * @param a reduced attraction parameter
+   * @param b reduced repulsion parameter
+   * @param beta derivative of correction factor
+   * @return Scalar residual entropy
+   */
   static Scalar residualEntropy(const Scalar& z, const Scalar& a,
                                 const Scalar& b, const Scalar& beta) noexcept {
     using std::log;
@@ -73,11 +99,15 @@ struct SoaveRedlichKwongEosPolicy {
     return R * (log(z - b) + a / b * beta * log((z + b) / z));
   }
 
-  /// @brief Computes residual Helmholtz energy
-  /// @param[in] z Z-factor
-  /// @param[in] t Temperature
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
+  /**
+   * @brief Compute residual Helmholtz energy
+   *
+   * @param z Z-factor
+   * @param t temperature
+   * @param a reduced attraction parameter
+   * @param b reduced repulsion parameter
+   * @return Scalar residual Helmholtz energy
+   */
   static Scalar residualHelmholtzEnergy(const Scalar& z, const Scalar& t,
                                         const Scalar& a,
                                         const Scalar& b) noexcept {
@@ -85,44 +115,78 @@ struct SoaveRedlichKwongEosPolicy {
     constexpr auto R = gasConstant<Scalar>();
     return R * t * (log(z - b) + a / b * log((z + b) / z));
   }
+  //@}
 };
 
+/**
+ * @brief Soave-Redlich-Kwong EoS
+ *
+ * EoS proposed by Soave (1972).
+ *
+ * @tparam Scalar scalar
+ */
 template <typename Scalar>
 class SoaveRedlichKwongEos
     : public CubicEosBase<Scalar, SoaveRedlichKwongEosPolicy<Scalar>,
                           SoaveCorrectionPolicy<Scalar>> {
  public:
+  /// Base class
   using Base = CubicEosBase<Scalar, SoaveRedlichKwongEosPolicy<Scalar>,
                             SoaveCorrectionPolicy<Scalar>>;
-
+  /// @name Constructors
+  //@{
   SoaveRedlichKwongEos() = default;
 
+  /**
+   * @brief Construct a new Soave Redlich Kwong Eos object
+   *
+   * @param pc critical pressure
+   * @param tc critical temperature
+   * @param omega acentric factor
+   */
   SoaveRedlichKwongEos(const Scalar& pc, const Scalar& tc, const Scalar& omega)
       : Base{pc, tc, SoaveCorrectionPolicy{calcM(omega)}}, omega_{omega} {}
 
   SoaveRedlichKwongEos(const SoaveRedlichKwongEos&) = default;
   SoaveRedlichKwongEos(SoaveRedlichKwongEos&&) = default;
+  //@}
 
   SoaveRedlichKwongEos& operator=(const SoaveRedlichKwongEos&) = default;
   SoaveRedlichKwongEos& operator=(SoaveRedlichKwongEos&&) = default;
 
+  /**
+   * @brief Set acentric factor
+   *
+   * @param omega acentric factor
+   */
   void setAcentricFactor(const Scalar& omega) {
     omega_ = omega;
     this->correctionPolicy().m() = calcM(omega);
   }
 
  private:
+  /**
+   * @brief Compute coefficient m for SoaveCorrectionPolicy
+   *
+   * @param omega acentric factor
+   * @return Scalar coefficient m
+   */
   static Scalar calcM(const Scalar& omega) noexcept {
     return 0.48 + (1.574 - 0.176 * omega) * omega;
   }
 
-  Scalar omega_;
+  Scalar omega_;  ///< acentric factor
 };
 
-/// @brief Makes Soave-Redlich-Kwong EoS
-/// @param[in] pc Critical pressure
-/// @param[in] tc Critical temperature
-/// @param[in] omega Acentric factor
+/**
+ * @brief Make a new Soave-Redlich-Kwong EoS object
+ *
+ * @tparam Scalar scalar
+ * @param pc critical pressure
+ * @param tc critical temperature
+ * @param omega acentric factor
+ * @return SoaveRedlichKwongEos<Scalar>
+ */
 template <typename Scalar>
 inline SoaveRedlichKwongEos<Scalar> makeSoaveRedlichKwongEos(
     const Scalar& pc, const Scalar& tc, const Scalar& omega) {
