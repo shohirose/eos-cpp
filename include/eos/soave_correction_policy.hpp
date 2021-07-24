fa40d7733ae65bedd943d4b74cb9a9f4f7be593f
Sho Hirose <sho.hirose@gmail.com>
2021-07-25 00:02:52 +0900

    Update comments for SoaveCorrectionPolicy

--- include/eos/soave_correction_policy.hpp
+++ include/eos/soave_correction_policy.hpp
@@ -4,10 +4,21 @@
 
 namespace eos {
 
+/**
+ * @brief Correction factor proposed by Soave (1972).
+ *
+ * @tparam Scalar scalar
+ */
 template <typename Scalar>
 class SoaveCorrectionPolicy {
  public:
   SoaveCorrectionPolicy() = default;
+
+  /**
+   * @brief Construct a new Soave Correction Policy object
+   *
+   * @param m coefficient used to calculate a correction factor
+   */
   SoaveCorrectionPolicy(const Scalar& m) : m_{m} {}
 
   SoaveCorrectionPolicy(const SoaveCorrectionPolicy&) = default;
@@ -19,14 +30,28 @@ class SoaveCorrectionPolicy {
   const Scalar& m() const noexcept { return m_; }
   Scalar& m() noexcept { return m_; }
 
-  /// @param[in] tr Reduced temperature
+  /**
+   * @brief Compute the correction factor at given temperature
+   *
+   * @param tr reduced temperature
+   * @return Scalar correction factor for attraction parameter
+   */
   Scalar value(const Scalar& tr) const noexcept {
     using std::sqrt;
     const auto a = 1 + m_ * (1 - sqrt(tr));
     return a * a;
   }
 
-  /// @param[in] tr Reduced temperature
+  /**
+   * @brief Compute the logarithmic derivative of correction factor
+   *
+   * This function calculates the following derivative:
+   * \f[ \frac{d \ln \alpha}{d \ln T} \f]
+   * where \f$\alpha\f$ is the correction factor and \f$T\f$ is temperature.
+   *
+   * @param tr reduced temperature
+   * @return Scalar logarithmic derivative of correction factor
+   */
   Scalar derivative(const Scalar& tr) const noexcept {
     using std::sqrt;
     const auto sqrt_tr = sqrt(tr);
