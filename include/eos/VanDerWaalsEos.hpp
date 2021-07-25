bc2ab7c7dc1c5c1ecf36e6e5a29e8f70281617aa
Sho Hirose <sho.hirose@gmail.com>
2021-07-26 07:56:39 +0900

    Update comments for VanDerWaalsEos

--- include/eos/VanDerWaalsEos.hpp
+++ include/eos/VanDerWaalsEos.hpp
@@ -1,68 +1,90 @@
 #pragma once
 
 #include <array>  // std::array
-#include <cmath>  // std::exp, std::log
+#include <cmath>  // std::log
 
 #include "eos/CubicEosBase.hpp"              // eos::CubicEosBase
 #include "eos/IdentityCorrectionFactor.hpp"  // eos::IdentityCorrectionFactor
 
 namespace eos {
 
-/// @brief Policy for Van der Waals EoS
+/**
+ * @brief EoS policy for Van der Waals EoS
+ *
+ * @tparam Scalar scalar
+ */
 template <typename Scalar>
 struct VanDerWaalsEosPolicy {
+  /// Coefficient for attraction parameter
   static constexpr Scalar omegaA = 0.421875;
+  /// Coefficient for repulsion parameter
   static constexpr Scalar omegaB = 0.125;
 
-  // Static Functions
+  /// @name Public static Functions
+  ///@{
 
-  /// @brief Computes pressure at given temperature and volume.
-  /// @param[in] t Temperature
-  /// @param[in] v Volume
-  /// @param[in] a Attraction parameter
-  /// @param[in] b Repulsion parameter
-  /// @returns Pressure
+  /**
+   * @brief Compute pressure at given temperature and volume.
+   *
+   * @param t temperature
+   * @param v volume
+   * @param a attraction parameter
+   * @param b respulsion parameter
+   * @return Scalar pressure
+   */
   static Scalar pressure(const Scalar& t, const Scalar& v, const Scalar& a,
                          const Scalar& b) noexcept {
     constexpr auto R = gasConstant<Scalar>();
     return R * t / (v - b) - a / (v * v);
   }
 
-  /// @brief Computes coefficients of the cubic equation of Z-factor
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
-  /// @returns Coefficients of the cubic equation of z-factor
+  /**
+   * @brief Compute coefficients of the cubic equation of Z-factor
+   *
+   * @param a reduced attraction parameter
+   * @param b reduced repulsion parameter
+   * @return std::array<Scalar, 3> coefficients of the cubic equation of
+   * Z-factor
+   */
   static std::array<Scalar, 3> zfactorCubicEq(const Scalar& a,
                                               const Scalar& b) noexcept {
     return {-b - 1, a, -a * b};
   }
 
-  /// @brief Computes the natural logarithm of a fugacity coefficient
-  /// @param[in] z Z-factor
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
-  /// @returns The natural logarithm of a fugacity coefficient
+  /**
+   * @brief Compute the natural logarithm of a fugacity coefficient
+   * @param[in] z Z-factor
+   * @param[in] a reduced attraction parameter
+   * @param[in] b reduced repulsion parameter
+   * @return The natural log of a fugacity coefficient
+   */
   static Scalar lnFugacityCoeff(const Scalar& z, const Scalar& a,
                                 const Scalar& b) noexcept {
     using std::log;
     return z - 1 - log(z - b) - a / z;
   }
 
-  /// @brief Computes residual enthalpy
-  /// @param[in] z Z-factor
-  /// @param[in] t Temperature
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
+  /**
+   * @brief Compute residual enthalpy
+   * @param[in] z Z-factor
+   * @param[in] t temperature
+   * @param[in] a reduced attraction parameter
+   * @param[in] b reduced repulsion parameter
+   * @return Scalar residual enthalpy
+   */
   static Scalar residualEnthalpy(const Scalar& z, const Scalar& t,
                                  const Scalar& a, const Scalar& b) noexcept {
     constexpr auto R = gasConstant<Scalar>();
     return R * t * (z - 1 - a / z);
   }
 
-  /// @brief Computes residual entropy
-  /// @param[in] z Z-factor
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
+  /**
+   * @brief Compute residual entropy
+   * @param[in] z Z-factor
+   * @param[in] a reduced attraction parameter
+   * @param[in] b reduced repulsion parameter
+   * @return Scalar residual entropy
+   */
   static Scalar residualEntropy(const Scalar& z, const Scalar& a,
                                 const Scalar& b) noexcept {
     using std::log;
@@ -70,11 +92,14 @@ struct VanDerWaalsEosPolicy {
     return R * (log(z - b));
   }
 
-  /// @brief Computes residual Helmholtz energy
-  /// @param[in] z Z-factor
-  /// @param[in] t Temperature
-  /// @param[in] a Reduced attraction parameter
-  /// @param[in] b Reduced repulsion parameter
+  /**
+   * @brief Compute residual Helmholtz energy
+   * @param[in] z Z-factor
+   * @param[in] t temperature
+   * @param[in] a reduced attraction parameter
+   * @param[in] b reduced repulsion parameter
+   * @return Scalar residual Helmholtz energy
+   */
   static Scalar residualHelmholtzEnergy(const Scalar& z, const Scalar& t,
                                         const Scalar& a,
                                         const Scalar& b) noexcept {
@@ -82,30 +107,58 @@ struct VanDerWaalsEosPolicy {
     constexpr auto R = gasConstant<Scalar>();
     return R * t * (log(z - b) + a / z);
   }
+  /// @}
 };
 
+/**
+ * @brief Van der Waals EoS
+ *
+ * Van der Waals EoS is expressed by
+ * \f[
+ *    P = \frac{RT}{V - b} - \frac{a}{V^2}
+ * \f]
+ * where \f$P\f$ is pressure, \f$T\f$ is temperature, \f$V\f$ is volume, \f$a\f$
+ * is attraction parameter, \f$b\f$ is repulsion parameter, and \f$R\f$ is gas
+ * constant.
+ *
+ * @tparam Scalar
+ */
 template <typename Scalar>
 class VanDerWaalsEos : public CubicEosBase<Scalar, VanDerWaalsEosPolicy<Scalar>,
                                            IdentityCorrectionFactor> {
  public:
+  /// Base class
   using Base = CubicEosBase<Scalar, VanDerWaalsEosPolicy<Scalar>,
                             IdentityCorrectionFactor>;
-
+  /// @name Constructors
+  /// @{
   VanDerWaalsEos() = default;
 
+  /**
+   * @brief Construct a new VanDerWaalsEos object
+   *
+   * @param pc critical pressure
+   * @param tc critical temperature
+   */
   VanDerWaalsEos(const Scalar& pc, const Scalar& tc)
       : Base{pc, tc, IdentityCorrectionFactor{}} {}
 
   VanDerWaalsEos(const VanDerWaalsEos&) = default;
   VanDerWaalsEos(VanDerWaalsEos&&) = default;
+  /// @}
 
   VanDerWaalsEos& operator=(const VanDerWaalsEos&) = default;
   VanDerWaalsEos& operator=(VanDerWaalsEos&&) = default;
 };
 
-/// @brief Makes van der Waals EoS
-/// @param[in] pc Critical pressure
-/// @param[in] tc Critical temperature
+/**
+ * @brief Create a new VanDerWaalsEos object
+ *
+ * @tparam Scalar scalar
+ * @param[in] pc critical pressure
+ * @param[in] tc critical temperature
+ * @return VanDerWaalsEos<Scalar>
+ */
 template <typename Scalar>
 inline VanDerWaalsEos<Scalar> makeVanDerWaalsEos(const Scalar& pc,
                                                  const Scalar& tc) {
